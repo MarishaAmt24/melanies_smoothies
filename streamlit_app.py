ca4f7b37465094a89d8e401e069ed1b3c9acd60f
MarishaAmt24 <marisha.amatya@gmail.com>
2024-11-21 19:12:16 +0100

    Update streamlit_app.py
    request response set to sf_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ from snowflake.snowpark.functions import col
 import requests
 
 smoothiefroot_response= requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
